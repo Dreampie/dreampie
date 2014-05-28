02155d4fd903280f0cc4599af40ef068cc5b173e
Dreampie <wangrenhui1990@gmail.com>
2014-05-28 10:40:19 +0800

    多级树形数据转换

--- src/main/java/cn/dreampie/common/utils/tree/TreeUtils.java
+++ src/main/java/cn/dreampie/common/utils/tree/TreeUtils.java
@@ -26,6 +26,28 @@ public class TreeUtils {
         return nodes;
     }
 
+    public static List toTreeLevel(List params, int level) {
+        List nodes = null;
+        if (params.size() > 0) {
+            //优先级排序
+            Collections.sort(params, new Comparator<TreeNode>() {
+                @Override
+                public int compare(TreeNode rp1, TreeNode rp2) {
+                    return Long.compare(rp1.getParentId(), rp2.getParentId());
+                }
+            });
+            nodes = toTreeLevel(params, ((TreeNode) params.get(0)).getParentId(), level);
+        }
+        return nodes;
+    }
+
+    /**
+     * 无限级树形结构
+     *
+     * @param params
+     * @param pid
+     * @return
+     */
     public static List toTree(List params, long pid) {
         List nodes = Lists.newArrayList();
 
@@ -45,6 +67,37 @@ public class TreeUtils {
         return nodes;
     }
 
+    /**
+     * 两级树形数据
+     *
+     * @param params
+     * @param pid
+     * @return
+     */
+    public static List toTreeLevel(List params, long pid, int level) {
+        List nodes = Lists.newArrayList();
+
+        if (params != null && params.size() > 0) {
+            TreeNode node = null;//当前节点
+            for (int i = 0; i < params.size(); i++) {
+                node = (TreeNode) params.get(i);
+                if (node.getParentId() == pid) {
+                    nodes.add(node);
+                    params.remove(i);
+                    if (level > 1) {
+//                        System.out.println(node.getId() + ":" + level);
+                        node.setChildren(toTreeLevel(params, node.getId(), --level));
+                    } else {
+//                        System.out.println(node.getId() + ":" + level + ":" + nodes.size());
+                        nodes.addAll(toTreeLevel(params, node.getId(), --level));
+                    }
+                    level++;
+                    i--;
+                }
+            }
+        }
+        return nodes;
+    }
 //
 //    public static void main(String[] args) {
 //        List params = Lists.newArrayList();
