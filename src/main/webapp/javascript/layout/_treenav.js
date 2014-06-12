9c73329f6e893159d72752bb17eb3ada78045cf8
Dreampie <wangrenhui1990@gmail.com>
2014-06-12 15:25:05 +0800

    treenav

--- src/main/webapp/javascript/layout/_treenav.js
+++ src/main/webapp/javascript/layout/_treenav.js
@@ -2,8 +2,11 @@ $(function () {
     $(".treenav li").click(function () {
         var li = $(this);
         if (!li.parent().parent().is(".treenav")) {
+            $(".treenav li").removeClass("active");
+            li.parents(".treenav li").addClass("active");
             li.addClass("active");
             li.siblings().removeClass("active");
+            li.siblings().children("li.active").removeClass("active");
             var icon = li.find("i:first");
             var children = li.children("ul.nav");
             if (children.length > 0) {
